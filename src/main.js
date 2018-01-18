9888da48ceee1ad56caca55c68d1b44dc9a37b4a
beautifulsoup23 <zs@lendify.se>
2018-01-18 19:29:04 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Layout from './components/layout.vue'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
+
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+	mode: 'history',
+	router :[{
+		path: '/',
+		component: IndexPage
+	}]
+   
+})
+
+
+/* eslint-disable no-new */
+new Vue({ 
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
